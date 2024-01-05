242715a8e6706cc93a1844b6585913d14517c63c
Muhamed Shaban <muhamed.shaban@zeitfracht.de>
2024-01-05 03:39:40 +0100

    Axessing: Add DbContext

---
+++ Axessing/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Axessing.Models.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Axessing.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    public DbSet<Ticket> Tickets { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+        modelBuilder.Entity<Ticket>().HasKey(t => t.Id);
+        base.OnModelCreating(modelBuilder);
+    }
+}
