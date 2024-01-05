4948553c96fb354ad58bb1460e4df64fa160df3d
Muhamed Shaban <muhamed.shaban@zeitfracht.de>
2024-01-05 03:42:00 +0100

    Axessing: Add AutoMapper

---
+++ Axessing/Services/Mapper.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using Axessing.Models.Schema;
+
+namespace Axessing.Services;
+
+public class Mapper : Profile
+{
+    public Mapper()
+    {
+        CreateMap<Ticket, Ticket>();
+    }
+}
